2e8ff953b795f36f0d5ad0597b29de6d432fb547
WeomuCat <weomu.cat@protonmail.com>
2019-07-25 22:54:01 +0800

    Code cleanup.

--- core/src/com/mygdx/game/screens/game/ability/AbilityResetCondition.java
+++
@@ -1,5 +0,0 @@
-package com.mygdx.game.screens.game.ability;
-
-public interface AbilityResetCondition {
-	boolean check(boolean isOnCooldown);
-}
--- core/src/com/mygdx/game/screens/game/ability/AbilityUseCondition.java
+++
@@ -1,6 +0,0 @@
-package com.mygdx.game.screens.game.ability;
-
-public interface AbilityUseCondition {
-	// should only accept 1 or 0 as input.
-	boolean check(int using);
-}
