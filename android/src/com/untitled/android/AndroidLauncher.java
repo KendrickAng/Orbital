70e285ecbede8da156d9a06d4ad918ae1afc7baa
WeomuCat <weomu.cat@protonmail.com>
2019-08-22 20:13:58 +0800

    Added android gradle support.

--- android/src/com/untitled/android/AndroidLauncher.java
+++ android/src/com/untitled/android/AndroidLauncher.java
@@ -1,9 +1,10 @@
-package com.untitled;
+package com.untitled.android;
 
 import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.untitled.UntitledGame;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
